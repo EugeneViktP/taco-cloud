a0bb24001afb98518c45dfbc43a8363b4678c1dc
EugeneViktP <popoff.zhenya@yandex.ru>
2025-05-18 21:18:58 +0200

    Commit with mistake in Types.TIMESTAMP

--- src/main/java/sia/taco_cloud/web/OrderController.java
+++ src/main/java/sia/taco_cloud/web/OrderController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
+import sia.taco_cloud.data.OrderRepository;
 import sia.taco_cloud.domain.TacoOrder;
 
 @Slf4j
@@ -18,17 +19,35 @@ import sia.taco_cloud.domain.TacoOrder;
 @SessionAttributes("tacoOrder")
 public class OrderController {
 
+    private OrderRepository orderRepo;
+
+    public OrderController(OrderRepository orderRepo) {
+        this.orderRepo = orderRepo;
+    }
+
     @GetMapping("/current")
     public  String orderForm() {
         return "orderForm";
     }
 
     @PostMapping
-    public String processOrder(@Valid TacoOrder order, Errors errors, SessionStatus sessionStatus) {
+    public String processOrder(@Valid TacoOrder order, Errors errors,
+                               SessionStatus sessionStatus) {
 
         if (errors.hasErrors()) {
             return  "orderForm";
         }
+
+//        try {
+//            orderRepo.save(order);
+//            log.info("Order submitted: {}", order);
+//        } catch (DataAccessException e) {
+//            log.error("Failed to save order: {}", order, e);
+//            throw e; // или вернуть страницу с ошибкой
+//        }
+
+        log.debug("Attempting to save order: {}", order);
+        orderRepo.save(order);
 
         log.info("Order submitted: {}", order);
         sessionStatus.setComplete();
