2a178303d974427c7157c0a0beb354aacb566269
EugeneViktP <popoff.zhenya@yandex.ru>
2025-05-22 09:26:55 +0200

    added Bean for Ingredient repo

--- src/main/java/sia/taco_cloud/web/DesignTacoController.java
+++ src/main/java/sia/taco_cloud/web/DesignTacoController.java
@@ -4,6 +4,8 @@ package sia.taco_cloud.web;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -78,4 +80,22 @@ public class DesignTacoController {
                 .filter(x -> x.getType().equals(type))
                 .collect(Collectors.toList());
     }
+
+    @Bean
+    public CommandLineRunner dataLoader(IngredientRepository repo) {
+        return args -> {
+            repo.save(new Ingredient("FLTO", "Flour Tortilla", Type.WRAP));
+            repo.save(new Ingredient("COTO", "Corn Tortilla", Type.WRAP));
+            repo.save(new Ingredient("GRBF", "Ground Beef", Type.PROTEIN));
+            repo.save(new Ingredient("CARN", "Carnitas", Type.PROTEIN));
+            repo.save(new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES));
+            repo.save(new Ingredient("LETC", "Lettuce", Type.VEGGIES));
+            repo.save(new Ingredient("CHED", "Cheddar", Type.CHEESE));
+            repo.save(new Ingredient("JACK", "Monterrey Jack", Type.CHEESE));
+            repo.save(new Ingredient("SLSA", "Salsa", Type.SAUCE));
+            repo.save(new Ingredient("SRCR", "Sour Cream", Type.SAUCE));
+        };
+    }
+
+
 }
